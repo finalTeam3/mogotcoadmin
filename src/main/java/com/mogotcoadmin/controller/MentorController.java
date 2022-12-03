de88014d2d8bba34ee86dadac7963ff5ed8a3c92
parkpirate <skbsy153@gmail.com>
2022-12-03 23:57:07 +0900

    멘토 승인시 메일 발송 완료_2022.12.03 혜정

--- src/main/java/com/mogotcoadmin/controller/MentorController.java
+++ src/main/java/com/mogotcoadmin/controller/MentorController.java
@@ -3,13 +3,18 @@ package com.mogotcoadmin.controller;
 
 import java.util.List;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mogotcoadmin.dto.MentorDTO;
 import com.mogotcoadmin.dto.UserDTO;
@@ -29,6 +34,9 @@ public class MentorController {
 	
 	@Autowired
 	UserService service1;
+	
+	@Autowired
+	JavaMailSender mailSender;
 
 	@RequestMapping("/get")
 	public String main(Model model) {
@@ -65,7 +73,7 @@ public class MentorController {
 		MentorDTO mdto = null;
 		try {
 			mtdto = service.get(mentor.getMentorid());
-			mdto = new MentorDTO(mtdto.getMentorid(), mtdto.getUserid(), mentor.getAdminid(), mtdto.getMentorcom(), mtdto.getMentorcon(), mtdto.getMentorimg(), mtdto.getMcardimg(), 1, mtdto.getMentordate(), 3, mtdto.getMentorcareer(), mtdto.getMentorapply(), mtdto.getMcardposition(), mtdto.getMpimg(), mtdto.getMcimg(), mtdto.getMentoring_mentoringid(), mtdto.getMentoring_mtitle(), mtdto.getMentoring_mentoringimg(), mtdto.getMentoring_mentoringdate(), mtdto.getMentoring_mplace(), mtdto.getUser_username());
+			mdto = new MentorDTO(mtdto.getMentorid(), mtdto.getUserid(), mentor.getAdminid(), mtdto.getMentorcom(), mtdto.getMentorcon(), mtdto.getMentorimg(), mtdto.getMcardimg(), 1, mtdto.getMentordate(), 3, mtdto.getMentorcareer(), mtdto.getMentorapply(), mtdto.getMcardposition(), mtdto.getMpimg(), mtdto.getMcimg(), mtdto.getMentoring_mentoringid(), mtdto.getMentoring_mtitle(), mtdto.getMentoring_mentoringimg(), mtdto.getMentoring_mentoringdate(), mtdto.getMentoring_mplace(), mtdto.getUser_username(), mtdto.getUser_useremail());
 			service.adminupdate(mdto);
 			
 			UserDTO before = null;
@@ -93,13 +101,39 @@ public class MentorController {
 		return "redirect:detail?mentorid=" + mentor.getMentorid();
 	}
 	
+	@RequestMapping("/emailsend")
+	@ResponseBody
+	public String emailsend(String email) throws Exception {
+		
+		// 메일 정보 세팅
+		String setFrom = "mogotcomentoring@gmail.com";	// 보내는 이
+		String toMail = email;	// 받는 이
+		String title = "MOGOTCO 멘토 승인 결과 안내입니다."; 	// 메일 제목
+		String content = "멘토가 된 것을 축하드립니다! 지금 멘토링을 등록해보세요~"; 	// 메일 내용
+		
+		// 단순 텍스트 메일 전송용
+		MimeMessage msg = mailSender.createMimeMessage();
+		MimeMessageHelper msgHelper = new MimeMessageHelper(msg, "UTF-8");
+		
+		// 메일 작성
+		msgHelper.setFrom("상부3조 <mogotcomentoring@gmail.com>");
+		msgHelper.setTo(toMail);
+		msgHelper.setSubject(title);
+		msgHelper.setText(content);
+		
+		// 메일 보내기
+		mailSender.send(msg);
+		
+		return "main";
+	}
+	
 	@RequestMapping("/cancelimpl")
 	public String cancelimpl(Model model, MentorDTO mentor) {
 		MentorDTO mtdto = null;
 		MentorDTO mdto = null;
 		try {
 			mtdto = service.get(mentor.getMentorid());
-			mdto = new MentorDTO(mtdto.getMentorid(), mtdto.getUserid(), mentor.getAdminid(), mtdto.getMentorcom(), mtdto.getMentorcon(), mtdto.getMentorimg(), mtdto.getMcardimg(), 0, mentor.getMentordate(), 3, mtdto.getMentorcareer(), mtdto.getMentorapply(), mtdto.getMcardposition(), mtdto.getMpimg(), mtdto.getMcimg(), mtdto.getMentoring_mentoringid(), mtdto.getMentoring_mtitle(), mtdto.getMentoring_mentoringimg(), mtdto.getMentoring_mentoringdate(), mtdto.getMentoring_mplace(), mtdto.getUser_username());
+			mdto = new MentorDTO(mtdto.getMentorid(), mtdto.getUserid(), mentor.getAdminid(), mtdto.getMentorcom(), mtdto.getMentorcon(), mtdto.getMentorimg(), mtdto.getMcardimg(), 0, mentor.getMentordate(), 3, mtdto.getMentorcareer(), mtdto.getMentorapply(), mtdto.getMcardposition(), mtdto.getMpimg(), mtdto.getMcimg(), mtdto.getMentoring_mentoringid(), mtdto.getMentoring_mtitle(), mtdto.getMentoring_mentoringimg(), mtdto.getMentoring_mentoringdate(), mtdto.getMentoring_mplace(), mtdto.getUser_username(), mtdto.getUser_useremail());
 			service.adminupdate(mdto);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
